6f8eca158151b8956ff0f8ed2c6b96e34801eee0
mamasidikova <sevara.mamasiidkova@mail.ru>
2022-04-19 18:31:42 +0300

    bot_api

--- homework.py
+++ homework.py
@@ -4,6 +4,7 @@ import logging
 import requests
 import telegram
 from dotenv import load_dotenv
+from http import HTTPStatus
 
 load_dotenv()
 
@@ -29,10 +30,27 @@ def send_message(bot, message):
 
 
 def get_api_answer(current_timestamp):
+    """Запрос к единственному эндпоинту API-сервиса.
+    В качестве параметра функция получает временную метку.
+    В случае успешного запроса должна вернуть ответ API,
+    преобразовав его из формата JSON к типам данных Python.
+    """
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    try:
+        homework_statuse = requests.get(ENDPOINT,
+                                         headers=HEADERS,
+                                         params=params
+                                         )
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к эндпоинту API-сервиса: {error}')
+        raise Exception(f'Ошибка при запросе к к эндпоинту API-сервиса: {error}')
+    if homework_statuse.status_code != HTTPStatus.OK:
+        status_code = homework_statuse.status_code
+        logging.error(f'Ошибка {status_code}')
+        raise Exception(f'Ошибка {status_code}')
+    else:
+        return homework_statuse.json()
 
 
 def check_response(response):
@@ -54,24 +72,19 @@ def parse_status(homework):
 
 
 def check_tokens():
-    ...
+    """Проверка доступности всех переменных окружения."""
+    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
 def main():
     """Основная логика работы бота."""
+    if not check_tokens():
+        logging.critical('TOKEN_NOT_FOUND')
+        raise Exception('Отсутствуют одна или несколько переменных окружения')
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
-    url = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-    payload = {'from_date': current_timestamp}
-
-    homework_statuses = requests.get(url, headers=headers, params=payload)
-    print(homework_statuses.json()) 
-
-
-    ...
-
+    bot = telegram.Bot(token=TELEGRAM_TOKEN) #инициализируем бота
+    current_timestamp = int(time.time()) #текущее время
+    
     while True:
         try:
             response = ...
